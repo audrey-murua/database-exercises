6a9e0229d313824b22b1c203cf39f2327c157057
audrey murua <audrey.murua1@gmail.com>
2022-02-11 09:34:47 -0600

    completed where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,3 +13,40 @@ WHERE last_name LIKE 'E%';
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%';
+
+# PART TWO
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya';
+
+SELECT first_name, last_name, gender
+FROM employees
+WHERE gender = 'M'
+    AND (first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya');
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%E';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+   AND last_name LIKE '%E';
+
+# OTHER SOLUTION FOR #4
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
