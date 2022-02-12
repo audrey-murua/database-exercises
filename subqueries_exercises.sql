3eacb17fd3cb820ad7b29c4be3ec6b2c4ba0ab1f
audrey murua <audrey.murua1@gmail.com>
2022-02-12 15:25:46 -0600

    fixed errors, added another solution and bonus to completed exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -14,7 +14,8 @@ WHERE emp_no IN (
     SELECT emp_no
     FROM employees
     WHERE first_name = 'Aamod'
-    );
+    )
+GROUP BY title;
 
 SELECT first_name, last_name
 FROM employees
@@ -22,5 +23,40 @@ WHERE emp_no IN (
     SELECT emp_no
     FROM dept_manager
     WHERE gender = 'F'
+    AND to_date > CURDATE()
     );
 
+# Other Solution #3
+
+SELECT first_name, last_name
+FROM employees
+WHERE gender = 'F'
+AND emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > CURDATE()
+);
+
+# Bonus
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE to_date > CURDATE()
+      AND emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+    ));
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM salaries
+    WHERE salary = (SELECT MAX(salary) FROM salaries)
+    AND to_date > CURDATE()
+);
